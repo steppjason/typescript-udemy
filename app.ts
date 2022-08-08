a8189fbf83aa73b355c671629a3db068319e8db8
Jason Stepp <hi@jasonstepp.me>
2022-08-08 14:46:05 -0700

    Tuples

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-const person: {name: string, age: number, hobbies: string[]} = {
+const person: { name: string, age: number, hobbies: string[], role: [number, string] } = {
 	name: 'Jason',
 	age: 36,
-	hobbies: ['Video Games', 'Reading']
+	hobbies: ['Video Games', 'Reading'],
+	role: [2, 'author']
 }
 
 console.log(person)
