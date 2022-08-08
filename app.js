dbb7723c1610f7bed341ad8aea6dd959f335c2a6
Jason Stepp <hi@jasonstepp.me>
2022-08-07 21:18:40 -0700

    Sample add function

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Code goes here...");
+function add(x, y) {
+    return x + y;
+}
+var num1 = '5';
+var num2 = 2.8;
+var result = add(num1, num2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Code goes here...")
+
+function add(x:number, y:number)
+{
+	return x + y
+}
+
+const num1 = 5
+const num2 = 2.8
+
+const result = add(num1, num2)
+
+console.log(result)
