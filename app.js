3c9b8fa483b90c654ddce48970077010d0fb42a5
Jason Stepp <hi@jasonstepp.me>
2022-08-08 15:30:46 -0700

    Never type

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var userInput;
 var userName;
 userInput = 5;
-userInput = 'Jason';
+userInput = "Jason";
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured', 500);
--- app.ts
+++ app.ts
@@ -2,4 +2,10 @@ let userInput: unknown
 let userName: string
 
 userInput = 5
-userInput = 'Jason'
+userInput = "Jason"
+
+function generateError(message: string, code: number) {
+	throw { message: message, errorCode: code }
+}
+
+generateError('An error occured', 500)
