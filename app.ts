92c183a6de25f1f2c8e70ebe4092543c889874c0
Jason Stepp <hi@jasonstepp.me>
2022-08-08 15:05:27 -0700

    Literal types

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
-function add(n1: number | string, n2: number | string) {
+function add(n1: number | string, n2: number | string, resultConversion: 'as-number' | 'as-text') {
 	let result
-	if (typeof n1 === "number" && typeof n2 === "number") result = n1 + n2
+	if (typeof n1 === "number" && typeof n2 === "number" || resultConversion === 'as-number')
+		result = +n1 + +n2
 	else result = n1.toString() + n2.toString()
 	return result
 }
 
-console.log(add(1, 2))
-console.log(add('Hello', ' World'))
-console.log(add(1, 'Testing'))
+console.log(add(1, 2, 'as-number'))
+console.log(add('Hello', ' World', 'as-text'))
+console.log(add(1, 'Testing', 'as-text'))
