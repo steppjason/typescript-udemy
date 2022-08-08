5a2fcee3bb198e21f5b8985609be1cafde050385
Jason Stepp <hi@jasonstepp.me>
2022-08-08 15:24:26 -0700

    Function types

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, callBack) {
+    var result = n1 + n2;
+    callBack(result);
+}
 printResult(add(5, 12));
 var combinedValues;
 combinedValues = add;
 console.log(combinedValues(8, 8));
+addAndHandle(1, 2, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -6,9 +6,18 @@ function printResult(num: number) {
 	console.log("Result: " + num)
 }
 
+function addAndHandle(n1: number, n2: number, callBack: (num: number) => void) {
+	const result = n1 + n2
+	callBack(result)
+}
+
 printResult(add(5, 12))
 
-let combinedValues: Function
+let combinedValues: (a: number, b: number) => number
 combinedValues = add
 
-console.log(combinedValues(8,8))
+console.log(combinedValues(8, 8))
+
+addAndHandle(1, 2, (result) => {
+	console.log(result)
+})
