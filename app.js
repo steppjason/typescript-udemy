be705278e252bd403015e6c823aaff40ccdae7e8
Jason Stepp <hi@jasonstepp.me>
2022-08-10 19:07:16 -0700

    Access modifiers

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 "use strict";
 class Department {
     constructor(name) {
+        this.employees = [];
         this.name = name;
     }
+    describe() {
+        console.log("Department: " + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
-const accounting = new Department('Accounting');
-console.log(accounting);
+const accounting = new Department("Accounting");
+accounting.addEmployee('Jason');
+accounting.addEmployee('Anna');
+accounting.printEmployeeInformation();
--- app.ts
+++ app.ts
@@ -1,11 +1,27 @@
 class Department {
 	name: string
+	private employees: string[] = []
 
 	constructor(name: string) {
 		this.name = name
 	}
+
+	describe(this: Department) {
+		console.log("Department: " + this.name)
+	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee)
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length)
+		console.log(this.employees)
+	}
 }
 
-const accounting = new Department('Accounting')
+const accounting = new Department("Accounting")
+accounting.addEmployee('Jason')
+accounting.addEmployee('Anna')
 
-console.log(accounting)
+accounting.printEmployeeInformation()
